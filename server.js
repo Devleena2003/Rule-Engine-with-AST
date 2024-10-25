b71ae9299fd8a5f7d5cc088c70901e208e4b9be4
Devleena Das <95465835+Devleena2003@users.noreply.github.com>
2024-10-25 20:36:43 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const ruleRoutes = require("./routes/ruleRoutes");
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+mongoose
+  .connect(
+    "mongodb+srv://devleena2003:rule-engine@cluster0.xz1go.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+  });
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route for serving the index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/index.html"));
+});
+app.use("/api/rules", ruleRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
